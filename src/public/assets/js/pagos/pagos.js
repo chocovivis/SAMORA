00e798cc7d1274d9134118295a58838a73680155
chocovivis <19161403@itoaxaca.edu.mx>
2022-11-29 00:28:17 -0600

    feature habitación

--- src/public/assets/js/pagos/pagos.js
+++ src/public/assets/js/pagos/pagos.js
@@ -1,16 +1,24 @@
-const BASE_URL = "/catalogo/habitacion";
 var $botones = document.getElementsByTagName('button');
 
+
 for(const boton of $botones){
+  if(boton.innerText=="true"){
+    boton.innerText="Disponible"
+  }else{
+    boton.innerText="Ocupado"
+  }
     boton.addEventListener("click", () => {
-          if(boton.outerText=="true"){
+          if(boton.outerText=="Disponible"){
             location.href = "/pago";
           }else{
             alert("Esta habitación no se puede reservar");
           }
-        
      });
 }
+
+
+
+
 
 
   

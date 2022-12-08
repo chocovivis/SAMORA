050eba5a6bced06015b2e5563f650a9456cc5513
chocovivis <19161403@itoaxaca.edu.mx>
2022-12-08 17:26:13 -0600

    intento de deploy 2

--- src/routes/servicios.route.ts
+++ src/routes/servicios.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { servicioResponse } from "../controllers/servicio.controller";
+import { validaRutaEmpleado } from "../middlewares/empleado.middleware";
 
 
 const servicioRouter: Router = Router();
 
-servicioRouter.get("/", servicioResponse);
+servicioRouter.get("/",validaRutaEmpleado,servicioResponse);
 
 export default servicioRouter;
 
